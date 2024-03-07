e9bbf9e8a9798b05538a6a3a116f78d2c1e0a693
Stefan Hynst <stefan@hynst.at>
2024-03-07 23:34:54 +0100

    Ask before expense deletion (#102)

---
+++ src/components/delete-popup.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { Trash2 } from 'lucide-react'
+import { AsyncButton } from './async-button'
+import { Button } from './ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog'
+
+export function DeletePopup({ onDelete }: { onDelete: () => Promise<void> }) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="destructive">
+          <Trash2 className="w-4 h-4 mr-2" />
+          Delete
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogTitle>Delete this expense?</DialogTitle>
+        <DialogDescription>
+          Do you really want to delete this expense? This action is
+          irreversible.
+        </DialogDescription>
+        <DialogFooter>
+          <AsyncButton
+            type="button"
+            variant="destructive"
+            loadingContent="Deleting…"
+            action={onDelete}
+          >
+            Yes
+          </AsyncButton>
+          <Button variant={'secondary'}>
+            <DialogClose>Cancel</DialogClose>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
