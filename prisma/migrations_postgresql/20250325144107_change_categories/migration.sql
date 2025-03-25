556c5ce392637bb710bc657d84046022b193249d
Stefan Hynst <stefan@hynst.at>
2025-03-25 21:45:43 +0100

    Change categories

---
+++ prisma/migrations_postgresql/20250325144107_change_categories/migration.sql
@@ -0,0 +1,6 @@
+UPDATE "Expense" SET "categoryId" = 200 WHERE "categoryId" IN (201, 202);
+
+INSERT INTO "Category" ("id", "grouping", "name", "icon") VALUES (105, 'Life', 'Cats', 'Cat');
+UPDATE "Category" SET "name" = 'Alma' WHERE "id" = 104;
+UPDATE "Category" SET "name" = 'Krido', "icon" = 'Flower' WHERE "id" = 201;
+DELETE FROM "Category" WHERE "id" = 202;
